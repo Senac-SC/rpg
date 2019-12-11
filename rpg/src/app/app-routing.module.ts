d73d56b1a916843655a689f40320bff482f21ed9
Nubiia <nubia.kscatone@gmail.com>
2019-12-11 17:10:42 -0300

    a
    umas coisinhas ai

--- rpg/src/app/app-routing.module.ts
+++ rpg/src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },  {
+    path: 'grupos',
+    loadChildren: () => import('./grupos/grupos.module').then( m => m.GruposPageModule)
   }
+
 ];
 
 @NgModule({
